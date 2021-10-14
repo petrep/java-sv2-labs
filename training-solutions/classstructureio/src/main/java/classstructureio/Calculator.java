344529fbdf5ea146f9ee5c7e5adf8ab24901a096
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-15 00:46:25 +0200

    Calculator class created, I tested it and it works as expected

--- training-solutions/classstructureio/src/main/java/classstructureio/Calculator.java
+++ training-solutions/classstructureio/src/main/java/classstructureio/Calculator.java
@@ -1,7 +1,20 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("calc");
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println(" Üdv a csodálatos összeadó programban!");
+		System.out.println("Kérlek írd be az első számot, lécci" +
+				" egész szám legyen: ");
+		int firstInt = scanner.nextInt();
+		System.out.println("Oké, és most kérlek írd be a második számot," +
+				" lécci ez is egész szám legyen: ");
+		int secondInt = scanner.nextInt();
+
+		System.out.println("" + firstInt + " + " + secondInt);
+		System.out.println(firstInt + secondInt);
 	}
 }
