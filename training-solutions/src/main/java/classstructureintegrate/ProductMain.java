0478b30b979b356b853094a7bf2174657164582c
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-18 00:06:22 +0200

    classstructureintegrate/Product+ProductMain exercise completed

---
+++ training-solutions/src/main/java/classstructureintegrate/ProductMain.java
@@ -0,0 +1,26 @@
+package classstructureintegrate;
+
+import java.util.Scanner;
+
+public class ProductMain {
+	public static void main(String[] args) {
+
+
+	Scanner scanner = new Scanner(System.in);
+
+	System.out.println("Kérlek add meg a termék nevét:");
+	String productName = scanner.nextLine();
+	System.out.println("Most pedig kérlek add meg a termék árát:");
+	int productPrice = scanner.nextInt();
+	scanner.nextLine();
+	Product myProduct = new Product(productName, productPrice);
+
+	System.out.println(myProduct.getName() + " " + myProduct.getPrice());
+
+		myProduct.increasePrice(300);
+	System.out.println(myProduct.getName() + " " + myProduct.getPrice());
+
+		myProduct.decreasePrice(100);
+	System.out.println(myProduct.getName() + " " + myProduct.getPrice());
+	}
+}
