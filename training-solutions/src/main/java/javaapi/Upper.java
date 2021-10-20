0b9bff812561be2c5929469502f1fcbe1266e3a3
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-20 13:04:09 +0200

    Upper exercise completed

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,10 @@
+package javaapi;
+
+import java.util.Locale;
+
+public class Upper {
+	public static void main(String[] args) {
+		String myString = "Hello World!";
+		System.out.println(myString.toUpperCase());
+	}
+}
