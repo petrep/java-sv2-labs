91d6398af236f9172866b53c83f335a8c83adc61
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-23 20:59:46 +0200

    ArrayOfArrays exercise completed

---
+++ training-solutions/src/main/java/arrayofarrays/ArrayOfArrays.java
@@ -0,0 +1,20 @@
+package arrayofarrays;
+
+public class ArrayOfArrays {
+	public void printArrayOfArrays(int[][] a) {
+		for (int i[] : a) {
+			for (int j : i) {
+				System.out.print(j);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		ArrayOfArrays arrayOfArrays = new ArrayOfArrays();
+		int[][] myArray = {{0, 1, 2}, {0, 1, 2}, {0, 1, 2}, {0, 1, 2}};
+
+		arrayOfArrays.printArrayOfArrays(myArray);
+	}
+}
