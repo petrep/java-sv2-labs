ba950fc52386675e16638ffa8496dea6e987c377
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-24 21:45:42 +0200

    MathMethods exercise completed

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,20 @@
+package math.math;
+
+public class MathMethods {
+	public static void main(String[] args) {
+		System.out.println(Math.max(2, 1));
+		System.out.println(Math.min(2, 1));
+		System.out.println(Math.PI);
+		System.out.println(Math.round(Math.PI));
+		System.out.println(Math.round(2.154));
+		System.out.println(Math.abs(-11.4));
+		System.out.println(Math.negateExact(1));
+		System.out.println(Math.addExact(1, 2));
+		System.out.println(Math.subtractExact(2, 1));
+		System.out.println(Math.multiplyExact(1, 2));
+		System.out.println(Math.pow(1.0, 2.0));
+		System.out.println(Math.incrementExact(1));
+		System.out.println(Math.decrementExact(2));
+		System.out.println(Math.random());
+	}
+}
