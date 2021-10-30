c7624d4b73f63838a3825905e3e33c7c59996622
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-30 12:19:44 +0200

    Index of numbers exercise completed

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -7,8 +7,18 @@ public class Division {
 		}
 		System.out.println("");
 	}
+
+	public void getRightNumbers(int[] numbers) {
+		for (int i = 1; i < numbers.length; i++) {
+			if (numbers[i] % (i) == 0) System.out.print(numbers[i] + ", ");
+		}
+	}
+
 	public static void main(String[] args) {
 		Division division = new Division();
 		division.getDivisors(12);
+
+		int[] numbers = {2, 5, 23, 56, -36, 45, 78, 14, -8, 4, 10};
+		division.getRightNumbers(numbers);
 	}
 }
