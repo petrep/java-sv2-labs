f3576362fcd6ec9824e02b872ad59823f58638bd
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-30 10:02:12 +0200

    LiteralsMain exercise completed

---
+++ training-solutions/src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,21 @@
+package literals;
+
+public class LiteralsMain {
+	public static void main(String[] args) {
+		System.out.println("1".concat("2"));
+		System.out.println("" + 1 + 2);
+		System.out.println(Integer.toString(1) + Integer.toString(2));
+
+		double quotient = 3d / 4d;
+		System.out.println(quotient);
+
+		long big = 3_244_444_444L;
+		System.out.println(big);
+
+		String word = "title".toUpperCase();
+		System.out.println(word);
+
+		System.out.println(Integer.toBinaryString(1));
+		System.out.println(Integer.toBinaryString(-2));
+	}
+}
