1c87dfcd18858e6b92e992d02f97d09c98bf52c8
peterpetrekanics <peter.petrekanics@liferay.com>
2021-11-25 13:13:48 +0100

    TwoLetters exercise completed

---
+++ training-solutions/src/main/java/algorithmstransformation/letters/TwoLetters.java
@@ -0,0 +1,2 @@
+package algorithmstransformation.letters;public class TwoLetters {
+}
---
+++ training-solutions/src/test/java/algorithmstransformation/letters/TwoLettersTest.java
@@ -0,0 +1,6 @@
+package algorithmstransformation.letters;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TwoLettersTest {
+  
+}
